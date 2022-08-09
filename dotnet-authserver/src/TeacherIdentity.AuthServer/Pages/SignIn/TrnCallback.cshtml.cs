cab24650142a2f57ed04852e9abad141a34ab27c
James Gunn <james@gunn.io>
2022-08-09 11:18:50 +0100

    Initial set of tests for the Email & TRN callback endpoints

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/SignIn/TrnCallback.cshtml.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/SignIn/TrnCallback.cshtml.cs
@@ -11,13 +11,16 @@ namespace TeacherIdentity.AuthServer.Pages.SignIn
     {
         private readonly FindALostTrnIntegrationHelper _findALostTrnIntegrationHelper;
         private readonly TeacherIdentityServerDbContext _dbContext;
+        private readonly ILogger<TrnCallbackModel> _logger;
 
         public TrnCallbackModel(
             FindALostTrnIntegrationHelper findALostTrnIntegrationHelper,
-            TeacherIdentityServerDbContext dbContext)
+            TeacherIdentityServerDbContext dbContext,
+            ILogger<TrnCallbackModel> logger)
         {
             _findALostTrnIntegrationHelper = findALostTrnIntegrationHelper;
             _dbContext = dbContext;
+            _logger = logger;
         }
 
         public async Task<IActionResult> OnGetAsync()
@@ -27,7 +30,7 @@ namespace TeacherIdentity.AuthServer.Pages.SignIn
             var requestUrl = UriHelper.GetEncodedUrl(Request);
             if (!_findALostTrnIntegrationHelper.ValidateCallback(requestUrl, out var findALostTrnUser))
             {
-                throw new Exception("Invalid callback from Find a lost TRN service.");
+                return BadRequest();
             }
 
             // We don't expect to have an existing user at this point
@@ -36,9 +39,11 @@ namespace TeacherIdentity.AuthServer.Pages.SignIn
                 throw new NotImplementedException();
             }
 
-            // TODO Do we propagate email address from Find?
+            if (!RequiredClaimsAreProvided(findALostTrnUser, "trn", Claims.GivenName, Claims.FamilyName))
+            {
+                return BadRequest();
+            }
 
-            CheckClaimsProvided(findALostTrnUser, "trn", Claims.GivenName, Claims.FamilyName);
             authenticationState.Trn = findALostTrnUser.FindFirst("trn")!.Value;
             authenticationState.FirstName = findALostTrnUser.FindFirst(Claims.GivenName)!.Value;
             authenticationState.LastName = findALostTrnUser.FindFirst(Claims.FamilyName)!.Value;
@@ -60,15 +65,18 @@ namespace TeacherIdentity.AuthServer.Pages.SignIn
 
             return await HttpContext.SignInUser(user);
 
-            static void CheckClaimsProvided(ClaimsPrincipal principal, params string[] claimTypes)
+            bool RequiredClaimsAreProvided(ClaimsPrincipal principal, params string[] claimTypes)
             {
                 foreach (var claimType in claimTypes)
                 {
                     if (!principal.HasClaim(c => c.Type == claimType))
                     {
-                        throw new Exception($"Required claim '{claimType}' was not provided.");
+                        _logger.LogError("Required claim '{ClaimType}' was not provided.", claimType);
+                        return false;
                     }
                 }
+
+                return true;
             }
         }
     }
---
+++ dotnet-authserver/test/TeacherIdentity.AuthServer.Tests/TestBase.CommonTests.cs
@@ -0,0 +1,20 @@
+﻿using Flurl;
+using TeacherIdentity.AuthServer.State;
+
+namespace TeacherIdentity.AuthServer.Tests;
+
+public partial class TestBase
+{
+    public async Task InvalidAuthenticationState_ReturnsBadRequest(HttpMethod method, string url)
+    {
+        // Arrange
+        var fullUrl = new Url(url).RemoveQueryParam(AuthenticationStateMiddleware.IdQueryParameterName);
+        var request = new HttpRequestMessage(method, fullUrl);
+
+        // Act
+        var response = await HttpClient.SendAsync(request);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status400BadRequest, (int)response.StatusCode);
+    }
+}
