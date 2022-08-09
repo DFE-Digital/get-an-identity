43bcd3f195e30bdffa291ab1c76e894905700350
James Gunn <james@gunn.io>
2022-08-09 10:37:26 +0100

    Add helper methods for asserting on HTML responses

---
+++ dotnet-authserver/test/TeacherIdentity.AuthServer.Tests/HttpResponseMessageExtensions.cs
@@ -0,0 +1,15 @@
+﻿using AngleSharp;
+using AngleSharp.Html.Dom;
+
+namespace TeacherIdentity.AuthServer.Tests;
+
+public static class HttpResponseMessageExtensions
+{
+    public static async Task<IHtmlDocument> GetDocument(this HttpResponseMessage response)
+    {
+        var content = await response.Content.ReadAsStringAsync();
+
+        var browsingContext = BrowsingContext.New(AngleSharp.Configuration.Default);
+        return (IHtmlDocument)await browsingContext.OpenAsync(req => req.Content(content));
+    }
+}
