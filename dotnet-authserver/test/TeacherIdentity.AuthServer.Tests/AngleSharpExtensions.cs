159872c3c1f0b4907c414e5ed96ef43e37e7cbd6
James Gunn <james@gunn.io>
2022-08-11 11:30:14 +0100

    Add helper methods for accessing elements by data-testid

---
+++ dotnet-authserver/test/TeacherIdentity.AuthServer.Tests/AngleSharpExtensions.cs
@@ -0,0 +1,25 @@
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+
+namespace TeacherIdentity.AuthServer.Tests;
+
+public static class AngleSharpExtensions
+{
+    public static T? As<T>(this IElement element)
+        where T : class, IElement
+    {
+        return element as T;
+    }
+
+    public static IReadOnlyList<IElement> GetAllElementsByTestId(this IElement element, string testId) =>
+        element.QuerySelectorAll($"*[data-testid='{testId}']").ToList();
+
+    public static IElement? GetElementByTestId(this IElement element, string testId) =>
+        GetAllElementsByTestId(element, testId).SingleOrDefault();
+
+    public static IReadOnlyList<IElement> GetAllElementsByTestId(this IHtmlDocument doc, string testId) =>
+        doc.Body!.GetAllElementsByTestId(testId);
+
+    public static IElement? GetElementByTestId(this IHtmlDocument doc, string testId) =>
+        doc.Body!.GetElementByTestId(testId);
+}
