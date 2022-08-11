50a438645480a4447915b26051e41e4a5af9e817
James Gunn <james@gunn.io>
2022-08-11 11:09:58 +0100

    Add a mechanism for signing in a user from a test

---
+++ dotnet-authserver/test/TeacherIdentity.AuthServer.Tests/SignInUserMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeacherIdentity.AuthServer.Models;
+
+namespace TeacherIdentity.AuthServer.Tests;
+
+public class SignInUserMiddleware
+{
+    private readonly TeacherIdentityServerDbContext _dbContext;
+
+    public SignInUserMiddleware(RequestDelegate next, TeacherIdentityServerDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var userId = Guid.Parse(context.Request.Form["UserId"]);
+        var user = await _dbContext.Users.SingleAsync(u => u.UserId == userId);
+        await context.SignInUser(user);
+    }
+}
