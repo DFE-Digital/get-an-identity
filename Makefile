61ba2a119b0ec7812395d79ba47cfe283f37710d
Venkata Dorisala <venkata.dorisala@doris-tech.com>
2022-08-12 12:54:26 +0100

    Infra resources added for the service Get an Identity.

--- Makefile
+++ Makefile
@@ -32,12 +32,48 @@ production:
 	$(eval ENV_SHORT=pd)
 	$(eval ENV_TAG=prod)
 
+read-keyvault-config:
+	$(eval KEY_VAULT_NAME=$(shell jq -r '.key_vault_name' terraform/workspace_variables/$(DEPLOY_ENV).tfvars.json))
+	$(eval KEY_VAULT_SECRET_NAME=INFRASTRUCTURE)
+
 set-azure-account: ${environment}
 	echo "Logging on to ${AZURE_SUBSCRIPTION}"
 	az account set -s ${AZURE_SUBSCRIPTION}
 
 tags: ##Tags that will be added to resource group on it's creation in ARM template
 	$(eval RG_TAGS=$(shell echo '{"Portfolio": "Early years and Schools Group", "Parent Business":"Teaching Regulation Agency", "Product" : "Get an Identity", "Service Line": "Teaching Workforce", "Service": "Teacher Services", "Service Offering": "Get an Identity", "Environment" : "$(ENV_TAG)"}' | jq . ))
+
+.PHONY: install-fetch-config
+install-fetch-config: ## Install the fetch-config script, for viewing/editing secrets in Azure Key Vault
+	[ ! -f bin/fetch_config.rb ] \
+		&& curl -s https://raw.githubusercontent.com/DFE-Digital/bat-platform-building-blocks/master/scripts/fetch_config/fetch_config.rb -o bin/fetch_config.rb \
+		&& chmod +x bin/fetch_config.rb \
+		|| true
+
+edit-keyvault-secret: read-keyvault-config install-fetch-config set-azure-account
+	bin/fetch_config.rb -s azure-key-vault-secret:${KEY_VAULT_NAME}/${KEY_VAULT_SECRET_NAME} \
+		-e -d azure-key-vault-secret:${KEY_VAULT_NAME}/${KEY_VAULT_SECRET_NAME} -f yaml -c
+
+print-keyvault-secret: read-keyvault-config install-fetch-config set-azure-account
+	bin/fetch_config.rb -s azure-key-vault-secret:${KEY_VAULT_NAME}/${KEY_VAULT_SECRET_NAME} -f yaml
+
+validate-keyvault-secret: read-keyvault-config install-fetch-config set-azure-account
+	bin/fetch_config.rb -s azure-key-vault-secret:${KEY_VAULT_NAME}/${KEY_VAULT_SECRET_NAME} -d quiet \
+		&& echo Data in ${KEY_VAULT_NAME}/${KEY_VAULT_SECRET_NAME} looks valid
+
+terraform-init:
+	$(if $(or $(DISABLE_PASSCODE),$(PASSCODE)), , $(error Missing environment variable "PASSCODE", retrieve from https://login.london.cloud.service.gov.uk/passcode))
+	[[ "${SP_AUTH}" != "true" ]] && az account show && az account set -s $(AZURE_SUBSCRIPTION) || true
+	terraform -chdir=terraform init -backend-config workspace_variables/${DEPLOY_ENV}.backend.tfvars $(backend_config) -upgrade -reconfigure
+
+terraform-plan: terraform-init
+	terraform -chdir=terraform plan -var-file workspace_variables/${DEPLOY_ENV}.tfvars.json
+
+terraform-apply: terraform-init
+	terraform -chdir=terraform apply -var-file workspace_variables/${DEPLOY_ENV}.tfvars.json ${AUTO_APPROVE}
+
+terraform-destroy: terraform-init
+	terraform -chdir=terraform destroy -var-file workspace_variables/${DEPLOY_ENV}.tfvars.json ${AUTO_APPROVE}
 
 deploy-azure-resources: set-azure-account tags # make dev deploy-azure-resources CONFIRM_DEPLOY=1
 	$(if $(CONFIRM_DEPLOY), , $(error can only run with CONFIRM_DEPLOY))
---
+++ terraform/app.tf
@@ -0,0 +1,138 @@
+resource "azurerm_service_plan" "service-plan" {
+  name                = "getanid-authserver-${var.environment_name}-app-spln"
+  location            = data.azurerm_resource_group.group.location
+  resource_group_name = data.azurerm_resource_group.group.name
+  os_type             = "Linux"
+  sku_name            = var.app_service_plan_sku
+
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
+}
+
+resource "azurerm_linux_web_app" "auth-server-app" {
+  name                = local.get_an_identity_app_name
+  location            = data.azurerm_resource_group.group.location
+  resource_group_name = data.azurerm_resource_group.group.name
+  service_plan_id     = azurerm_service_plan.service-plan.id
+  https_only          = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    http2_enabled       = true
+    minimum_tls_version = "1.2"
+  }
+
+  app_settings = {
+    HOSTING_ENVIRONMENT                    = var.environment_name,
+    APPLICATION_INSIGHTS_CONNECTION_STRING = azurerm_application_insights.insights.connection_string
+    POSTGRES_CONNECTION_STRING             = "Host=${local.postgres_server_name};Username=${local.infrastructure_secrets.POSTGRES_ADMIN_USERNAME};Password=${local.infrastructure_secrets.POSTGRES_ADMIN_PASSWORD};Database=${local.postgres_database_name}"
+    REDIS_URL                              = "${azurerm_redis_cache.redis.hostname}/${azurerm_redis_cache.redis.primary_access_key}",
+    AZURE_STORAGE_ACCOUNT_NAME             = azurerm_storage_account.data-protection.name,
+    AZURE_STORAGE_ACCESS_KEY               = azurerm_storage_account.data-protection.primary_access_key,
+    AZURE_STORAGE_CONTAINER                = azurerm_storage_container.uploads.name
+  }
+
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
+}
+
+resource "azurerm_linux_web_app" "test-server-app" {
+  count               = var.environment_name == "dev" ? 1 : 0
+  name                = local.get_an_identity_test_client_name
+  location            = data.azurerm_resource_group.group.location
+  resource_group_name = data.azurerm_resource_group.group.name
+  service_plan_id     = azurerm_service_plan.service-plan.id
+  https_only          = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    http2_enabled       = true
+    minimum_tls_version = "1.2"
+  }
+
+  app_settings = {
+    HOSTING_ENVIRONMENT                    = var.environment_name,
+    APPLICATION_INSIGHTS_CONNECTION_STRING = azurerm_application_insights.insights.connection_string
+    POSTGRES_CONNECTION_STRING             = "Host=${local.postgres_server_name};Username=${local.infrastructure_secrets.POSTGRES_ADMIN_USERNAME};Password=${local.infrastructure_secrets.POSTGRES_ADMIN_PASSWORD};Database=${local.postgres_database_name}"
+    REDIS_URL                              = "${azurerm_redis_cache.redis.hostname}/${azurerm_redis_cache.redis.primary_access_key}",
+    AZURE_STORAGE_ACCOUNT_NAME             = azurerm_storage_account.data-protection.name,
+    AZURE_STORAGE_ACCESS_KEY               = azurerm_storage_account.data-protection.primary_access_key,
+    AZURE_STORAGE_CONTAINER                = azurerm_storage_container.uploads.name
+  }
+
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
+}
+
+resource "azurerm_postgresql_flexible_server" "postgres-server" {
+  name                   = local.postgres_server_name
+  location               = data.azurerm_resource_group.group.location
+  resource_group_name    = data.azurerm_resource_group.group.name
+  version                = 12
+  administrator_login    = local.infrastructure_secrets.POSTGRES_ADMIN_USERNAME
+  administrator_password = local.infrastructure_secrets.POSTGRES_ADMIN_PASSWORD
+  zone                   = 1
+  create_mode            = "Default"
+  storage_mb             = var.postgres_flexible_server_storage_mb
+  sku_name               = var.postgres_flexible_server_sku
+
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
+}
+
+resource "azurerm_postgresql_flexible_server_database" "postgres-database" {
+  name      = local.postgres_database_name
+  server_id = azurerm_postgresql_flexible_server.postgres-server.id
+}
+
+resource "azurerm_redis_cache" "redis" {
+  name                = local.redis_database_name
+  location            = data.azurerm_resource_group.group.location
+  resource_group_name = data.azurerm_resource_group.group.name
+  capacity            = var.redis_service_capacity
+  family              = var.redis_service_family
+  sku_name            = var.redis_service_sku
+  enable_non_ssl_port = false
+  minimum_tls_version = "1.2"
+  redis_version       = var.redis_service_version
+
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
+
+}
+
+resource "azurerm_application_insights" "insights" {
+  name                 = "getanid-${var.environment_name}-appinsights"
+  location             = data.azurerm_resource_group.group.location
+  resource_group_name  = data.azurerm_resource_group.group.name
+  application_type     = "web"
+  daily_data_cap_in_gb = var.application_insights_daily_data_cap_mb
+  retention_in_days    = var.application_insights_retention_days
+
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
+}
