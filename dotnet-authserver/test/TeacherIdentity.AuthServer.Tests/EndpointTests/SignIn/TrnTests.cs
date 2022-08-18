0eeac46eab305dac15815d809417c35f8aba560a
James Gunn <james@gunn.io>
2022-08-18 10:44:56 +0100

    Add tests for trn endpoint

---
+++ dotnet-authserver/test/TeacherIdentity.AuthServer.Tests/EndpointTests/SignIn/TrnTests.cs
@@ -0,0 +1,38 @@
+﻿namespace TeacherIdentity.AuthServer.Tests.EndpointTests.SignIn;
+
+public class TrnTests : TestBase
+{
+    public TrnTests(HostFixture hostFixture)
+        : base(hostFixture)
+    {
+    }
+
+    [Fact]
+    public async Task Get_NoAuthenticationStateProvided_ReturnsBadRequest()
+    {
+        await InvalidAuthenticationState_ReturnsBadRequest(HttpMethod.Get, $"/sign-in/trn");
+    }
+
+    [Fact]
+    public async Task Get_ValidRequest_ReturnsOk()
+    {
+        // Arrange
+        var authStateHelper = CreateAuthenticationStateHelper();
+
+        var request = new HttpRequestMessage(HttpMethod.Get, $"/sign-in/trn?{authStateHelper.ToQueryParam()}");
+
+        // Act
+        var response = await HttpClient.SendAsync(request);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status200OK, (int)response.StatusCode);
+    }
+
+    private AuthenticationStateHelper CreateAuthenticationStateHelper() =>
+        CreateAuthenticationStateHelper(authState =>
+        {
+            authState.EmailAddress = Faker.Internet.Email();
+            authState.EmailAddressConfirmed = true;
+            authState.FirstTimeUser = true;
+        });
+}
