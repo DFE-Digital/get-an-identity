f375cde7cd8a40166633c74577697323bd17ce34
James Gunn <james@gunn.io>
2022-08-18 10:44:47 +0100

    Amend trn column size to 7

---
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Migrations/20220818094257_TrnLookupStateTrnLength.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeacherIdentity.AuthServer.Migrations
+{
+    public partial class TrnLookupStateTrnLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "trn",
+                table: "journey_trn_lookup_states",
+                type: "character(7)",
+                fixedLength: true,
+                maxLength: 7,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "trn",
+                table: "journey_trn_lookup_states",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character(7)",
+                oldFixedLength: true,
+                oldMaxLength: 7,
+                oldNullable: true);
+        }
+    }
+}
