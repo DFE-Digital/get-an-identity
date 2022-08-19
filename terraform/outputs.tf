053ef65611f7d3b42d373cf1c0fdebcca26163ad
Merril Mathew <merril.mathew@outlook.com>
2022-08-19 13:48:44 +0100

    Add swap slot for apps
    Get an Identity app deployments in preprod and production should use
    app service swap deployment capability. This ensures that apps are
    deployed and smoke tested before swapping to production slot. This
    commit adds terraform confiuration to deploy staging slot along with the
    apps in preprod and production.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,6 @@
 output "auth_server_fqdn" {
   value = azurerm_linux_web_app.auth-server-app.default_hostname
 }
+output "postgres_server_name" {
+  value = azurerm_postgresql_flexible_server.postgres-server.name
+}
---
+++ terraform/workspace_variables/production.backend.tfvars
@@ -0,0 +1,3 @@
+storage_account_name = "s165p01getanidtfstatepd"
+key                  = "production.tfstate"
+resource_group_name  = "s165p01-getanid-pd-rg"
