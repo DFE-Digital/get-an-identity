0b9dd2447836f2769e1d1541f6992e12b61d59fa
James Gunn <james@gunn.io>
2022-09-14 19:14:11 +0100

    Add helpers for testing with scopes

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Oidc/CustomScopes.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Oidc/CustomScopes.cs
@@ -6,6 +6,8 @@ public static class CustomScopes
     public const string GetAnIdentitySupport = "get-an-identity:support";
     public const string Trn = "trn";
 
+    public static string[] All => AdminScopes.Concat(TeacherScopes).ToArray();
+
     public static string[] AdminScopes { get; } = new[]
     {
         GetAnIdentityAdmin,
