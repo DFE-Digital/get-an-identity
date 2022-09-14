857ad86b42cd07a0fc6321f84f269bdeed51e942
James Gunn <james@gunn.io>
2022-09-14 19:14:11 +0100

    Add helpers for testing with scopes

---
+++ dotnet-authserver/test/TeacherIdentity.AuthServer.Tests/EndpointTests/Api/ScopeTheoryData.cs
@@ -0,0 +1,29 @@
+using TeacherIdentity.AuthServer.Oidc;
+
+namespace TeacherIdentity.AuthServer.Tests.EndpointTests.Api;
+
+public static class ScopeTheoryData
+{
+    public static TheoryData<string> GetAdminScopes() =>
+        FromScopes(CustomScopes.AdminScopes);
+
+    public static TheoryData<string> GetAllAdminScopesExcept(TheoryData<string> scopes) =>
+        FromScopes(CustomScopes.AdminScopes.Except(scopes.Select(d => (string)d.Single())).Append(""));
+
+    public static TheoryData<string> GetAllAdminScopesExcept(IEnumerable<string> scopes) =>
+        FromScopes(CustomScopes.AdminScopes.Except(scopes));
+
+    public static TheoryData<string> Single(string scope) => FromScopes(new[] { scope });
+
+    private static TheoryData<string> FromScopes(IEnumerable<string> scopes)
+    {
+        var data = new TheoryData<string>();
+
+        foreach (var scope in scopes)
+        {
+            data.Add(scope);
+        }
+
+        return data;
+    }
+}
