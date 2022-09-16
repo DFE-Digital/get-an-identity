7fb6063cd52f30c47b03e0317a076bd6c3ad3217
James Gunn <james@gunn.io>
2022-09-16 14:37:25 +0100

    Add Swagger annotations to API methods

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Api/V1/Responses/GetTeacherDetailResponse.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Api/V1/Responses/GetTeacherDetailResponse.cs
@@ -1,7 +1,20 @@
+using Swashbuckle.AspNetCore.Filters;
 using TeacherIdentity.AuthServer.Api.V1.ApiModels;
 
 namespace TeacherIdentity.AuthServer.Api.V1.Responses;
 
 public class GetTeacherDetailResponse : TeacherInfo
 {
+    public class TrnRequestInfoExample : IExamplesProvider<GetTeacherDetailResponse>
+    {
+        public GetTeacherDetailResponse GetExamples() => new()
+        {
+            UserId = new Guid("29e9e624-073e-41f5-b1b3-8164ce3a5233"),
+            DateOfBirth = new DateOnly(1990, 1, 1),
+            Email = "kevin.e@example.com",
+            FirstName = "Kevin",
+            LastName = "E",
+            Trn = "2921020"
+        };
+    }
 }
