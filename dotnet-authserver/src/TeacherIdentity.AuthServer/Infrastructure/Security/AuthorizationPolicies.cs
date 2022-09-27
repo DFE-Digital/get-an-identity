3e9df021706b8204e3f9a7abf91bda810724c85e
James Gunn <james@gunn.io>
2022-09-27 16:25:05 +0100

    Add an Admin area

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Infrastructure/Security/AuthorizationPolicies.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Infrastructure/Security/AuthorizationPolicies.cs
@@ -2,7 +2,7 @@ namespace TeacherIdentity.AuthServer.Infrastructure.Security;
 
 public static class AuthorizationPolicies
 {
+    public const string GetAnIdentityAdmin = "GetAnIdentityAdmin";
     public const string GetAnIdentitySupportApi = "API:GetAnIdentitySupport";
-    public const string Hangfire = "Hangfire";
     public const string TrnLookupApi = "API:TrnLookup";
 }
