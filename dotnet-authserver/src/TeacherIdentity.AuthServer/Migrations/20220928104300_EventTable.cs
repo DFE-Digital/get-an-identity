1c6c30ea313b47b68c8689b158eb55246fd1934a
James Gunn <james@gunn.io>
2022-09-28 12:28:47 +0100

    Add initial Event system for capturing data changes (#167)
    For now simply persist every event into our DB.

---
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Migrations/20220928104300_EventTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TeacherIdentity.AuthServer.Migrations
+{
+    public partial class EventTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "events",
+                columns: table => new
+                {
+                    event_id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    event_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    created = table.Column<DateTime>(type: "jsonb", nullable: false),
+                    payload = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_events", x => x.event_id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "events");
+        }
+    }
+}
