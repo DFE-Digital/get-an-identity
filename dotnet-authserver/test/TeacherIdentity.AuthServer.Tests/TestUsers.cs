51c57b9467a8715132b0a0bbe5dfacd1bef358f8
James Gunn <james@gunn.io>
2022-09-28 14:14:33 +0100

    Add pages for adding a new Staff user and viewing Staff users

--- dotnet-authserver/test/TeacherIdentity.AuthServer.Tests/TestUsers.cs
+++ dotnet-authserver/test/TeacherIdentity.AuthServer.Tests/TestUsers.cs
@@ -16,9 +16,21 @@ public static class TestUsers
         StaffRoles = StaffRoles.All
     };
 
+    public static User AdminUserWithNoRoles { get; } = new User()
+    {
+        UserId = new Guid("1a049cf0-6c0f-4157-a77d-4b081145a05a"),
+        Created = DateTime.UtcNow,
+        DateOfBirth = DateOnly.FromDateTime(Faker.Identification.DateOfBirth()),
+        EmailAddress = Faker.Internet.Email(),
+        FirstName = Faker.Name.First(),
+        LastName = Faker.Name.Last(),
+        UserType = UserType.Staff,
+        StaffRoles = StaffRoles.None
+    };
+
     public static async Task CreateUsers(TeacherIdentityServerDbContext dbContext)
     {
-        var allUsers = new[] { AdminUserWithAllRoles };
+        var allUsers = new[] { AdminUserWithAllRoles, AdminUserWithNoRoles };
 
         foreach (var user in allUsers)
         {
