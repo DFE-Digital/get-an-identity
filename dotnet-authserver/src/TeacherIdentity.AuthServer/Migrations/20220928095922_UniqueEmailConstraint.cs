90ca8c23b9f2ccd09f65cf5918756e8038e0d327
James Gunn <james@gunn.io>
2022-09-28 11:05:44 +0100

    Fix unique email constraint

---
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Migrations/20220928095922_UniqueEmailConstraint.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeacherIdentity.AuthServer.Migrations
+{
+    public partial class UniqueEmailConstraint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_users_email_address",
+                table: "users");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_email_address",
+                table: "users",
+                column: "email_address",
+                unique: true,
+                filter: "is_deleted = false");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_users_email_address",
+                table: "users");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_email_address",
+                table: "users",
+                column: "email_address",
+                unique: true,
+                filter: "is_deleted = true");
+        }
+    }
+}
