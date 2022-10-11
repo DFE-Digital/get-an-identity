e73eb5ea867354b33dc6f8dd02c9fb85c0b2682f
James Gunn <james@gunn.io>
2022-10-11 15:22:25 +0100

    Amend WebHookId type to Guid

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Models/Mappings/WebHookMapping.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Models/Mappings/WebHookMapping.cs
@@ -8,7 +8,7 @@ public class WebHookMapping : IEntityTypeConfiguration<WebHook>
     public void Configure(EntityTypeBuilder<WebHook> builder)
     {
         builder.ToTable("webhooks");
-        builder.Property(w => w.WebHookId).ValueGeneratedOnAdd();
+        builder.Property(w => w.WebHookId).IsRequired();
         builder.Property(w => w.Endpoint).IsRequired().HasMaxLength(200);
         builder.Property(w => w.Endpoint).IsRequired();
         builder.HasKey(w => w.WebHookId);
