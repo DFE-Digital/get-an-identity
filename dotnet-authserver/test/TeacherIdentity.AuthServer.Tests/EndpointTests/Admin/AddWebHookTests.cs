7fae2a5e974af39d006546343bf594361a56a92d
James Gunn <james@gunn.io>
2022-10-11 17:20:08 +0100

    Add UI for managing web hooks

---
+++ dotnet-authserver/test/TeacherIdentity.AuthServer.Tests/EndpointTests/Admin/AddWebHookTests.cs
@@ -0,0 +1,93 @@
+using Microsoft.EntityFrameworkCore;
+using TeacherIdentity.AuthServer.Events;
+
+namespace TeacherIdentity.AuthServer.Tests.EndpointTests.Admin;
+
+public class AddWebHookTests : TestBase
+{
+    public AddWebHookTests(HostFixture hostFixture)
+        : base(hostFixture)
+    {
+    }
+
+    [Fact]
+    public async Task Get_UnauthenticatedUser_RedirectsToSignIn()
+    {
+        await UnauthenticatedUser_RedirectsToSignIn(HttpMethod.Get, "/admin/webhooks/new");
+    }
+
+    [Fact]
+    public async Task Get_AuthenticatedUserDoesNotHavePermission_ReturnsForbidden()
+    {
+        await AuthenticatedUserDoesNotHavePermission_ReturnsForbidden(HttpMethod.Get, "/admin/webhooks/new");
+    }
+
+    [Fact]
+    public async Task Get_ValidRequest_RendersExpectedContent()
+    {
+        // Arrange
+        var request = new HttpRequestMessage(HttpMethod.Get, "/admin/webhooks/new");
+
+        // Act
+        var response = await AuthenticatedHttpClient!.SendAsync(request);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status200OK, (int)response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Post_UnauthenticatedUser_RedirectsToSignIn()
+    {
+        await UnauthenticatedUser_RedirectsToSignIn(HttpMethod.Post, "/admin/webhooks/new");
+    }
+
+    [Fact]
+    public async Task Post_AuthenticatedUserDoesNotHavePermission_ReturnsForbidden()
+    {
+        await AuthenticatedUserDoesNotHavePermission_ReturnsForbidden(HttpMethod.Post, "/admin/webhooks/new");
+    }
+
+    [Fact]
+    public async Task Post_ValidRequest_CreatesWebHookEmitsEventAndRedirectsToWebHooksPage()
+    {
+        // Arrange
+        var endpoint = Faker.Internet.Url();
+        var enabled = true;
+
+        var request = new HttpRequestMessage(HttpMethod.Post, "/admin/webhooks/new")
+        {
+            Content = new FormUrlEncodedContentBuilder()
+                .Add("Endpoint", endpoint)
+                .Add("Enabled", enabled)
+                .ToContent()
+        };
+
+        // Act
+        var response = await AuthenticatedHttpClient!.SendAsync(request);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status302Found, (int)response.StatusCode);
+        Assert.Equal("/admin/webhooks", response.Headers.Location?.OriginalString);
+
+        var webHookId = await TestData.WithDbContext(async dbContext =>
+        {
+            var webHook = await dbContext.WebHooks.SingleOrDefaultAsync(u => u.Endpoint == endpoint);
+
+            Assert.NotNull(webHook);
+            Assert.Equal(enabled, webHook!.Enabled);
+
+            return webHook.WebHookId;
+        });
+
+        EventObserver.AssertEventsSaved(
+            e =>
+            {
+                var webHookAdded = Assert.IsType<WebHookAddedEvent>(e);
+                Assert.Equal(TestUsers.AdminUserWithAllRoles.UserId, webHookAdded.AddedByUserId);
+                Assert.Equal(Clock.UtcNow, webHookAdded.CreatedUtc);
+                Assert.Equal(webHookId, webHookAdded.WebHookId);
+                Assert.Equal(enabled, webHookAdded.Enabled);
+                Assert.Equal(endpoint, webHookAdded.Endpoint);
+            });
+    }
+}
--- dotnet-authserver/test/TeacherIdentity.AuthServer.Tests/EndpointTests/Admin/WebHooksTests.cs
+++ dotnet-authserver/test/TeacherIdentity.AuthServer.Tests/EndpointTests/Admin/WebHooksTests.cs
@@ -1,8 +1,8 @@
 namespace TeacherIdentity.AuthServer.Tests.EndpointTests.Admin;
 
-public class StaffTests : TestBase
+public class WebHooksTests : TestBase
 {
-    public StaffTests(HostFixture hostFixture)
+    public WebHooksTests(HostFixture hostFixture)
         : base(hostFixture)
     {
     }
@@ -10,20 +10,20 @@ public class StaffTests : TestBase
     [Fact]
     public async Task Get_UnauthenticatedUser_RedirectsToSignIn()
     {
-        await UnauthenticatedUser_RedirectsToSignIn(HttpMethod.Get, "/admin/staff");
+        await UnauthenticatedUser_RedirectsToSignIn(HttpMethod.Get, "/admin/webhooks");
     }
 
     [Fact]
     public async Task Get_AuthenticatedUserDoesNotHavePermission_ReturnsForbidden()
     {
-        await AuthenticatedUserDoesNotHavePermission_ReturnsForbidden(HttpMethod.Get, "/admin/staff");
+        await AuthenticatedUserDoesNotHavePermission_ReturnsForbidden(HttpMethod.Get, "/admin/webhooks");
     }
 
     [Fact]
     public async Task Get_ValidRequest_RendersExpectedContent()
     {
         // Arrange
-        var request = new HttpRequestMessage(HttpMethod.Get, "/admin/staff");
+        var request = new HttpRequestMessage(HttpMethod.Get, "/admin/webhooks");
 
         // Act
         var response = await AuthenticatedHttpClient!.SendAsync(request);
