11d096c360ef0dd06a10973b5662c15bfee11300
James Gunn <james@gunn.io>
2022-10-12 16:12:12 +0100

    Fix SQL column types for events table

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Models/Mappings/EventMapping.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Models/Mappings/EventMapping.cs
@@ -10,8 +10,9 @@ public class EventMapping : IEntityTypeConfiguration<Event>
         builder.ToTable("events");
         builder.Property<long>("event_id").IsRequired().ValueGeneratedOnAdd();
         builder.Property(e => e.EventName).IsRequired().HasMaxLength(200);
-        builder.Property(e => e.Created).IsRequired().HasColumnType("jsonb");
-        builder.Property(e => e.Payload).IsRequired();
+        builder.Property(e => e.Created).IsRequired();
+        builder.Property(e => e.Payload).IsRequired().HasColumnType("jsonb");
         builder.HasKey("event_id");
+        builder.HasIndex(e => e.Payload).HasMethod("gin");
     }
 }
