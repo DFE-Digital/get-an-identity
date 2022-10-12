ae4ab1ab490f650f016a786e943693883c242377
James Gunn <james@gunn.io>
2022-10-12 13:28:17 +0100

    Remove Message suffix from MessageType on UserSignedInMessage

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Notifications/Messages/UserSignedInMessage.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Notifications/Messages/UserSignedInMessage.cs
@@ -2,5 +2,7 @@ namespace TeacherIdentity.AuthServer.Notifications.Messages;
 
 public class UserSignedInMessage : INotificationMessage
 {
+    public const string MessageTypeName = "UserSignedIn";
+
     public required User User { get; init; }
 }
