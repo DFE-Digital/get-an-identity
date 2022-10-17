9923e4f8110df1adfc0e3256bf98585001a95f3b
James Gunn <james@gunn.io>
2022-10-17 16:51:57 +0100

    Sent notification when a user is updated

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Notifications/Messages/UserSignedInMessage.cs
+++
@@ -1,8 +0,0 @@
-namespace TeacherIdentity.AuthServer.Notifications.Messages;
-
-public class UserSignedInMessage : INotificationMessage
-{
-    public const string MessageTypeName = "UserSignedIn";
-
-    public required User User { get; init; }
-}
