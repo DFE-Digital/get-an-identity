f811d9929fedcae1229b9898bc85866ae515248c
James Gunn <james@gunn.io>
2023-01-13 17:11:10 +0000

    Add FindTeachers method to DqtApiClient (#361)

---
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Services/DqtApi/FindTeachersRequest.cs
@@ -0,0 +1,15 @@
+namespace TeacherIdentity.AuthServer.Services.DqtApi;
+
+public record FindTeachersRequest
+{
+    public string? FirstName { get; init; }
+    public string? LastName { get; init; }
+    public string? PreviousFirstName { get; init; }
+    public string? PreviousLastName { get; init; }
+    public DateOnly? DateOfBirth { get; init; }
+    public string? NationalInsuranceNumber { get; init; }
+    public string? IttProviderName { get; init; }
+    public string? IttProviderUkprn { get; init; }
+    public string? EmailAddress { get; init; }
+    public string? Trn { get; init; }
+}
--- dotnet-authserver/src/TeacherIdentity.AuthServer/Services/DqtApi/IDqtApiClient.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Services/DqtApi/IDqtApiClient.cs
@@ -2,5 +2,6 @@ namespace TeacherIdentity.AuthServer.Services.DqtApi;
 
 public interface IDqtApiClient
 {
+    Task<FindTeachersResponse> FindTeachers(FindTeachersRequest request, CancellationToken cancellationToken = default);
     public Task<TeacherInfo?> GetTeacherByTrn(string trn, CancellationToken cancellationToken = default);
 }
