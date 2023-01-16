c21d9db116c476a6f7deaf749c96a153db9a72fa
James Gunn <james@gunn.io>
2023-01-16 15:22:52 +0000

    Port Support User Detail page

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Services/DqtApi/TeacherInfo.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Services/DqtApi/TeacherInfo.cs
@@ -5,4 +5,6 @@ public record TeacherInfo
     public required string Trn { get; init; }
     public required string FirstName { get; init; }
     public required string LastName { get; init; }
+    public required DateOnly DateOfBirth { get; init; }
+    public required string? NationalInsuranceNumber { get; init; }
 }
