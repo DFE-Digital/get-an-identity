64d12c8f6aceb2820c3b3d42cc49ee3587784e8d
WILPOW <william.powell@softwire.com>
2023-01-25 15:20:02 +0000

    Create user on identity check answers form submission

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/SignIn/Trn/CheckAnswers.cshtml.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/SignIn/Trn/CheckAnswers.cshtml.cs
@@ -1,16 +1,28 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TeacherIdentity.AuthServer.Models;
+using TeacherIdentity.AuthServer.Models.Mappings;
+using TeacherIdentity.AuthServer.Services.EmailVerification;
 
 namespace TeacherIdentity.AuthServer.Pages.SignIn.Trn;
 
 public class CheckAnswers : PageModel
 {
-    private IIdentityLinkGenerator _linkGenerator;
+    private readonly IIdentityLinkGenerator _linkGenerator;
+    private readonly TeacherIdentityServerDbContext _dbContext;
+    private readonly IClock _clock;
 
-    public CheckAnswers(IIdentityLinkGenerator linkGenerator)
+    public CheckAnswers(
+        IIdentityLinkGenerator linkGenerator,
+        TeacherIdentityServerDbContext dbContext,
+        IClock clock)
     {
         _linkGenerator = linkGenerator;
+        _dbContext = dbContext;
+        _clock = clock;
     }
 
     public string BackLink => (HttpContext.GetAuthenticationState().HaveIttProvider == true)
@@ -31,8 +43,48 @@ public class CheckAnswers : PageModel
     {
     }
 
-    public void OnPost()
+    public async Task<IActionResult> OnPost()
     {
+        var authenticationState = HttpContext.GetAuthenticationState();
+
+        var userId = Guid.NewGuid();
+        var user = new User()
+        {
+            CompletedTrnLookup = _clock.UtcNow,
+            Created = _clock.UtcNow,
+            DateOfBirth = authenticationState.DateOfBirth,
+            EmailAddress = authenticationState.EmailAddress!,
+            FirstName = string.IsNullOrEmpty(authenticationState.FirstName) ? authenticationState.OfficialFirstName! : authenticationState.FirstName,
+            LastName = string.IsNullOrEmpty(authenticationState.LastName) ? authenticationState.OfficialLastName! : authenticationState.LastName,
+            Updated = _clock.UtcNow,
+            UserId = userId,
+            UserType = UserType.Default,
+            Trn = authenticationState.StatedTrn,
+            TrnAssociationSource = !string.IsNullOrEmpty(authenticationState.Trn) ? TrnAssociationSource.Lookup : null,
+            LastSignedIn = _clock.UtcNow,
+            RegisteredWithClientId = authenticationState.OAuthState?.ClientId,
+            TrnLookupStatus = !string.IsNullOrEmpty(authenticationState.Trn) ? TrnLookupStatus.Found : TrnLookupStatus.None
+        };
+
+        _dbContext.Users.Add(user);
+
+        _dbContext.AddEvent(new Events.UserRegisteredEvent()
+        {
+            ClientId = authenticationState.OAuthState?.ClientId,
+            CreatedUtc = _clock.UtcNow,
+            User = user
+        });
+
+        try
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+        catch (DbUpdateException dex) when (dex.IsUniqueIndexViolation("ix_users_email_address"))
+        {
+            return Redirect(_linkGenerator.TrnInUseChooseEmail());
+        }
+
+        return Redirect(authenticationState.GetNextHopUrl(_linkGenerator));
     }
 
     public override void OnPageHandlerExecuting(PageHandlerExecutingContext context)
@@ -42,7 +94,7 @@ public class CheckAnswers : PageModel
         // We expect to have a verified email and official names at this point but we shouldn't have completed the TRN lookup
         if (string.IsNullOrEmpty(authenticationState.EmailAddress) ||
             !authenticationState.EmailAddressVerified ||
-            !authenticationState.HasOfficialName() ||
+            string.IsNullOrEmpty(authenticationState.GetOfficialName()) ||
             authenticationState.HaveCompletedTrnLookup)
         {
             context.Result = new RedirectResult(authenticationState.GetNextHopUrl(_linkGenerator));
--- dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/SignIn/Trn/IttProvider.cshtml.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/SignIn/Trn/IttProvider.cshtml.cs
@@ -62,7 +62,7 @@ public class IttProvider : TrnLookupPageModel
         // We expect to have a verified email and official names at this point but we shouldn't have completed the TRN lookup
         if (string.IsNullOrEmpty(authenticationState.EmailAddress) ||
             !authenticationState.EmailAddressVerified ||
-            !authenticationState.HasOfficialName() ||
+            string.IsNullOrEmpty(authenticationState.GetOfficialName()) ||
             authenticationState.HaveCompletedTrnLookup)
         {
             context.Result = new RedirectResult(authenticationState.GetNextHopUrl(LinkGenerator));
