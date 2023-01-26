6928df2c56121c9c2d131e4d4ab0b37250a8436e
James Gunn <james@gunn.io>
2023-01-26 16:34:22 +0000

    Act on pen test recommendation for X-XSS-Protection header

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Infrastructure/Middleware/AppendSecurityResponseHeadersMiddleware.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Infrastructure/Middleware/AppendSecurityResponseHeadersMiddleware.cs
@@ -16,7 +16,7 @@ public class AppendSecurityResponseHeadersMiddleware
         if (!response.HasStarted)
         {
             response.Headers["X-Frame-Options"] = "DENY";
-            response.Headers["X-Xss-Protection"] = "1; mode=block";
+            response.Headers["X-Xss-Protection"] = "0";
             response.Headers["X-Content-Type-Options"] = "nosniff";
             response.Headers["X-Permitted-Cross-Domain-Policies"] = "none";
             response.Headers["Referrer-Policy"] = "no-referrer";
