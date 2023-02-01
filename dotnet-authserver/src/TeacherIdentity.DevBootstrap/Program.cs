fc1e017f31b73f0c47ec9d832388a6b765802a32
Andrew Horth <andrew@horth.co.uk>
2023-02-01 09:54:45 +0000

    Add utility function to import test users from CSV (#397)
    * Added ability to import a CSV of test users into the teacher identity database into the dev bootstrap utility
    
    * Fixed using order
    
    * Tweaks based on PR comments

--- dotnet-authserver/src/TeacherIdentity.DevBootstrap/Program.cs
+++ dotnet-authserver/src/TeacherIdentity.DevBootstrap/Program.cs
@@ -1,10 +1,14 @@
+using System.Globalization;
 using Bogus;
+using CsvHelper;
+using CsvHelper.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TeacherIdentity.AuthServer.EventProcessing;
 using TeacherIdentity.AuthServer.Models;
 using TeacherIdentity.AuthServer.Oidc;
+using TeacherIdentity.DevBootstrap.Csv;
 using static Bogus.DataSets.Name;
 
 var configuration = new ConfigurationManager();
@@ -47,6 +51,12 @@ if (args.Contains("--generate-test-users"))
 {
     Console.WriteLine();
     await GenerateTestUsers();
+}
+
+if (args.Contains("--import-test-users"))
+{
+    Console.WriteLine();
+    await ImportTestUsers();
 }
 
 async Task CreateDatabase()
@@ -153,6 +163,39 @@ async Task GenerateTestUsers()
         }
 
         await dbContext.SaveChangesAsync();
+    });
+
+    Console.WriteLine("done.");
+}
+
+async Task ImportTestUsers(string csvFileName = "test-users.csv")
+{
+    Console.Write("Importing test users... ");
+
+    using var reader = new StreamReader(csvFileName);
+    using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = true });
+    csv.Context.RegisterClassMap<UserReaderMap>();
+
+    await WithDbContext(async dbContext =>
+    {
+        int i = 0;
+        await foreach (var item in csv.GetRecordsAsync<User>())
+        {
+            dbContext.Users.Add(item);
+            if (i != 0 && i % 10_000 == 0)
+            {
+                await dbContext.SaveChangesAsync();
+                Console.WriteLine($"Saved {i} users in teacher identity database.");
+            }
+
+            i++;
+        }
+
+        if (dbContext.ChangeTracker.HasChanges())
+        {
+            await dbContext.SaveChangesAsync();
+            Console.WriteLine($"Saved {i} users in teacher identity database.");
+        }
     });
 
     Console.WriteLine("done.");
