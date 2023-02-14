a6e6834150af0cdc1370636bca8ece9aea2ba6c8
Andrew Horth <andrew@horth.co.uk>
2023-02-14 11:31:45 +0000

    Added table displaying list of user imports in UI

--- dotnet-authserver/tests/TeacherIdentity.AuthServer.Tests/EndpointTests/Admin/UserImportsTests.cs
+++ dotnet-authserver/tests/TeacherIdentity.AuthServer.Tests/EndpointTests/Admin/UserImportsTests.cs
@@ -1,3 +1,5 @@
+using TeacherIdentity.AuthServer.Models;
+
 namespace TeacherIdentity.AuthServer.Tests.EndpointTests.Admin;
 
 public class UserImportsTests : TestBase
@@ -24,11 +26,72 @@ public class UserImportsTests : TestBase
     {
         // Arrange
         var request = new HttpRequestMessage(HttpMethod.Get, "/admin/user-imports");
+        var userImportJobId = Guid.NewGuid();
+        var userImportJobRowSuccess1 = new UserImportJobRow
+        {
+            UserImportJobId = userImportJobId,
+            RowNumber = 1,
+            Id = Guid.NewGuid().ToString(),
+            UserId = Guid.NewGuid()
+        };
+
+        var userImportJobRowFailure = new UserImportJobRow
+        {
+            UserImportJobId = userImportJobId,
+            RowNumber = 2,
+            Id = Guid.NewGuid().ToString(),
+            Errors = new List<string> { "There is something wrong with this row" }
+        };
+
+        var userImportJobRowSuccess2 = new UserImportJobRow
+        {
+            UserImportJobId = userImportJobId,
+            RowNumber = 3,
+            Id = Guid.NewGuid().ToString(),
+            UserId = Guid.NewGuid()
+        };
+
+        var userImportJob = new UserImportJob
+        {
+            UserImportJobId = userImportJobId,
+            OriginalFilename = "my-test.csv",
+            StoredFilename = "stored.csv",
+            UserImportJobStatus = UserImportJobStatus.Processed,
+            Uploaded = DateTime.UtcNow,
+            UserImportJobRows = new List<UserImportJobRow>
+            {
+                userImportJobRowSuccess1,
+                userImportJobRowFailure,
+                userImportJobRowSuccess2,
+            }
+        };
+
+        await TestData.WithDbContext(async dbContext =>
+        {
+            dbContext.UserImportJobs.Add(userImportJob);
+            await dbContext.SaveChangesAsync();
+        });
 
         // Act
         var response = await HttpClient.SendAsync(request);
 
         // Assert
         Assert.Equal(StatusCodes.Status200OK, (int)response.StatusCode);
+
+        var doc = await response.GetDocument();
+        var tableRow = doc.GetElementByTestId($"user-import-{userImportJobId}");
+        Assert.NotNull(tableRow);
+        var file = tableRow.GetElementByTestId($"file-{userImportJobId}");
+        Assert.NotNull(file);
+        Assert.Equal(userImportJob.OriginalFilename, file.TextContent);
+        var uploaded = tableRow.GetElementByTestId($"uploaded-{userImportJobId}");
+        Assert.NotNull(uploaded);
+        Assert.Equal(userImportJob.Uploaded.ToString("dd/MM/yyyy HH:mm"), uploaded.TextContent);
+        var status = tableRow.GetElementByTestId($"status-{userImportJobId}");
+        Assert.NotNull(status);
+        Assert.Equal(userImportJob.UserImportJobStatus.ToString(), status.TextContent);
+        var summary = tableRow.GetElementByTestId($"summary-{userImportJobId}");
+        Assert.NotNull(summary);
+        Assert.Equal("2 / 3", summary.TextContent);
     }
 }
