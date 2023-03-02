9c0e31a460eccd95046cc0c38877bd001dadb38b
WILPOW <william.powell@softwire.com>
2023-03-02 15:20:11 +0000

    Add resend email confirmation page for register journey

---
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/SignIn/Register/ResendEmailConfirmation.cshtml.cs
@@ -0,0 +1,62 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TeacherIdentity.AuthServer.Models;
+using TeacherIdentity.AuthServer.Services.UserVerification;
+
+namespace TeacherIdentity.AuthServer.Pages.SignIn.Register;
+
+[BindProperties]
+public class ResendEmailConfirmationModel : BaseEmailPageModel
+{
+    public ResendEmailConfirmationModel(
+        IUserVerificationService userVerificationService,
+        IIdentityLinkGenerator linkGenerator,
+        TeacherIdentityServerDbContext dbContext) :
+        base(userVerificationService, linkGenerator, dbContext)
+    {
+    }
+
+    [Display(Name = "Email address")]
+    [Required(ErrorMessage = "Enter your email address")]
+    [EmailAddress(ErrorMessage = "Enter a valid email address")]
+    public string? Email { get; set; }
+
+    public void OnGet()
+    {
+        Email = HttpContext.GetAuthenticationState().EmailAddress;
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return this.PageWithErrors();
+        }
+
+        var validateEmailResult = await ValidateEmail(Email!);
+
+        if (!validateEmailResult.IsValid)
+        {
+            return validateEmailResult.Result!;
+        }
+
+        HttpContext.GetAuthenticationState().OnEmailSet(Email!);
+
+        return Redirect(LinkGenerator.RegisterEmailConfirmation());
+    }
+
+    public override void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+    {
+        var authenticationState = context.HttpContext.GetAuthenticationState();
+
+        if (authenticationState.EmailAddress is null)
+        {
+            context.Result = Redirect(LinkGenerator.RegisterEmail());
+        }
+        else if (authenticationState.EmailAddressVerified)
+        {
+            context.Result = Redirect(LinkGenerator.RegisterPhone());
+        }
+    }
+}
