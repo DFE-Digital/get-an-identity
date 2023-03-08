9ddd562f65c8a78611880ded3e817627ddad5055
wilpow-github <110819441+wilpow-github@users.noreply.github.com>
2023-03-08 15:01:57 +0000

    Add resend confirm existing account phone page for register journey (#458)

---
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/SignIn/Register/ResendConfirmExistingAccountPhone.cshtml.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TeacherIdentity.AuthServer.Services.UserVerification;
+
+namespace TeacherIdentity.AuthServer.Pages.SignIn.Register;
+
+public class ResendConfirmExistingAccountPhone : BaseExistingPhonePageModel
+{
+    private IIdentityLinkGenerator _linkGenerator;
+
+    public ResendConfirmExistingAccountPhone(
+        IUserVerificationService userVerificationService,
+        IIdentityLinkGenerator linkGenerator) :
+        base(userVerificationService)
+    {
+        _linkGenerator = linkGenerator;
+    }
+
+    public string? ExistingMobileNumber => HttpContext.GetAuthenticationState().ExistingAccountMobileNumber;
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return this.PageWithErrors();
+        }
+
+        var pinGenerationResult = await GenerateSmsPinForExistingMobileNumber(ExistingMobileNumber!);
+
+        if (!pinGenerationResult.Success)
+        {
+            return pinGenerationResult.Result!;
+        }
+
+        return Redirect(_linkGenerator.RegisterConfirmExistingAccountPhone());
+    }
+
+    public override void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+    {
+        var authenticationState = context.HttpContext.GetAuthenticationState();
+
+        if (authenticationState.ExistingAccountMobileNumber is null ||
+            authenticationState.ExistingAccountChosen != true)
+        {
+            context.Result = new RedirectResult(_linkGenerator.RegisterCheckAccount());
+        }
+    }
+}
