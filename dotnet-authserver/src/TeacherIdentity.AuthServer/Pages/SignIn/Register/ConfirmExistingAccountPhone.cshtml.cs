50ef73317e85de238ca6b410f2316d90b808f51a
WILPOW <william.powell@softwire.com>
2023-03-08 13:58:07 +0000

    Remove unused imports

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/SignIn/Register/ConfirmExistingAccountPhone.cshtml.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/SignIn/Register/ConfirmExistingAccountPhone.cshtml.cs
@@ -2,7 +2,6 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
-using TeacherIdentity.AuthServer.Helpers;
 using TeacherIdentity.AuthServer.Models;
 using TeacherIdentity.AuthServer.Services.UserVerification;
 
