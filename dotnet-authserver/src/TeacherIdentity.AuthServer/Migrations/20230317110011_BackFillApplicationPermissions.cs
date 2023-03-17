6d175132395f3b423a617e28eba249b9940a4929
WILPOW <william.powell@softwire.com>
2023-03-17 13:29:52 +0000

    Backfill phone scope

---
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Migrations/20230317110011_BackFillApplicationPermissions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeacherIdentity.AuthServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class BackFillApplicationPermissions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                UPDATE applications
+                SET permissions = REPLACE(permissions, 'scp:email', 'scp:email, scp:phone');
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                UPDATE applications
+                SET permissions = REPLACE(permissions, ', scp:phone', '');
+                """);
+        }
+    }
+}
