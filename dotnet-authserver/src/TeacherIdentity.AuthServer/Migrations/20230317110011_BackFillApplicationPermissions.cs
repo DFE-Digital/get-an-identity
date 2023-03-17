f618bb35715b9123c331816499f58276f6e4f2c4
WILPOW <william.powell@softwire.com>
2023-03-17 14:20:39 +0000

    Replace scp:phone if it appears at the start of the array

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Migrations/20230317110011_BackFillApplicationPermissions.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Migrations/20230317110011_BackFillApplicationPermissions.cs
@@ -21,7 +21,7 @@ namespace TeacherIdentity.AuthServer.Migrations
         {
             migrationBuilder.Sql("""
                 UPDATE applications
-                SET permissions = REPLACE(permissions, ', scp:phone', '');
+                SET permissions = REPLACE(REPLACE(permissions, 'scp:phone, ', ''), ', scp:phone', '');
                 """);
         }
     }
