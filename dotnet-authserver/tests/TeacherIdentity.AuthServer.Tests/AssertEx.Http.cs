0f931a602cccc5f2aa16dac770fdf86e63780499
James Gunn <james@gunn.io>
2023-04-18 17:44:41 +0100

    Back-port JSON test helper fixes from QT API

--- dotnet-authserver/tests/TeacherIdentity.AuthServer.Tests/AssertEx.Http.cs
+++ dotnet-authserver/tests/TeacherIdentity.AuthServer.Tests/AssertEx.Http.cs
@@ -7,10 +7,7 @@ public static partial class AssertEx
 {
     public static async Task<JsonDocument> JsonResponse(HttpResponseMessage response, int expectedStatusCode = StatusCodes.Status200OK)
     {
-        if (response is null)
-        {
-            throw new ArgumentNullException(nameof(response));
-        }
+        ArgumentNullException.ThrowIfNull(response);
 
         Assert.Equal(expectedStatusCode, (int)response.StatusCode);
         Assert.Equal("application/json", response.Content.Headers.ContentType?.MediaType);
@@ -18,6 +15,16 @@ public static partial class AssertEx
         var result = await response.Content.ReadFromJsonAsync<JsonDocument>(SerializerOptions);
         Assert.NotNull(result);
         return result!;
+    }
+
+    public static async Task JsonResponseEquals(HttpResponseMessage response, object expected, int expectedStatusCode = StatusCodes.Status200OK)
+    {
+        ArgumentNullException.ThrowIfNull(response);
+        ArgumentNullException.ThrowIfNull(expected);
+
+        var jsonDocument = await JsonResponse(response, expectedStatusCode);
+
+        JsonObjectEquals(expected, jsonDocument);
     }
 
     public static async Task JsonResponseIsError(HttpResponseMessage response, int expectedErrorCode, int expectedStatusCode)
@@ -43,12 +50,24 @@ public static partial class AssertEx
         Assert.Equal(expectedError, problemDetails.Errors?[propertyName].Single());
     }
 
+    public static async Task JsonResponseHasValidationErrorsForProperties(
+        HttpResponseMessage response,
+        IReadOnlyDictionary<string, string> expectedErrors,
+        int expectedStatusCode = 400)
+    {
+        var problemDetails = await ResponseIsProblemDetails(response, expectedStatusCode);
+
+        Assert.NotNull(problemDetails.Extensions);
+
+        foreach (var e in expectedErrors)
+        {
+            Assert.Equal(e.Value, problemDetails.Errors?[e.Key].Single());
+        }
+    }
+
     private static async Task<ProblemDetails> ResponseIsProblemDetails(HttpResponseMessage response, int expectedStatusCode)
     {
-        if (response is null)
-        {
-            throw new ArgumentNullException(nameof(response));
-        }
+        ArgumentNullException.ThrowIfNull(response);
 
         Assert.Equal(expectedStatusCode, (int)response.StatusCode);
         Assert.Equal("application/problem+json", response.Content.Headers.ContentType?.MediaType);
@@ -64,8 +83,23 @@ public static partial class AssertEx
     {
         public string? Title { get; set; }
         public int Status { get; set; }
+        [JsonConverter(typeof(ProblemDetailsErrorJsonConverter))]
         public IDictionary<string, string[]>? Errors { get; set; }
         [JsonExtensionData]
         public IDictionary<string, JsonElement>? Extensions { get; set; }
+    }
+
+    private class ProblemDetailsErrorJsonConverter : JsonConverter<IDictionary<string, string[]>>
+    {
+        public override IDictionary<string, string[]>? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var dic = (Dictionary<string, string[]>)JsonSerializer.Deserialize(ref reader, typeToConvert, options)!;
+            return new Dictionary<string, string[]>(dic, StringComparer.OrdinalIgnoreCase);
+        }
+
+        public override void Write(Utf8JsonWriter writer, IDictionary<string, string[]> value, JsonSerializerOptions options)
+        {
+            throw new NotSupportedException();
+        }
     }
 }
