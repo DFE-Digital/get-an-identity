ff56b13082cea7ac9f1157b278f6d810175737e9
James Gunn <james@gunn.io>
2023-04-19 12:42:42 +0100

    Use STJ for webhook message serialization (#541)

---
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Infrastructure/Json/OptionJsonConverter.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Optional;
+using Optional.Unsafe;
+
+namespace TeacherIdentity.AuthServer.Infrastructure.Json;
+
+public class OptionJsonConverter<T> : JsonConverter<Option<T>>
+{
+    public override Option<T> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var asT = JsonSerializer.Deserialize<T>(ref reader, options)!;
+        return Option.Some(asT);
+    }
+
+    public override void Write(Utf8JsonWriter writer, Option<T> value, JsonSerializerOptions options)
+    {
+        Debug.Assert(value.HasValue);  // Modifier should ensure we only get here if there's a value
+        JsonSerializer.Serialize<T>(writer, value.ValueOrFailure(), options);
+    }
+}
