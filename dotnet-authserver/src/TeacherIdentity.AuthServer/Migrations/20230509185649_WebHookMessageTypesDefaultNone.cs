ba03a6b17f674cf417579a5c47930f5196e33c34
James Gunn <james@gunn.io>
2023-05-09 19:57:21 +0100

    Amend the default WebHook.MessageTypes to None

---
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Migrations/20230509185649_WebHookMessageTypesDefaultNone.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeacherIdentity.AuthServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class WebHookMessageTypesDefaultNone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "web_hook_message_types",
+                table: "webhooks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 3);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "web_hook_message_types",
+                table: "webhooks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 3,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 0);
+        }
+    }
+}
