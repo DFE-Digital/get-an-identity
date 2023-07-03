ec5f42ddea267e3caafa2fd64a5cb59203ee0715
James Gunn <james@gunn.io>
2023-07-03 15:39:04 +0100

    Shorten TRN tokens

---
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Migrations/20230703143539_VariableSizeTrnToken.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeacherIdentity.AuthServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class VariableSizeTrnToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "trn_token",
+                table: "trn_tokens",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character(128)",
+                oldFixedLength: true,
+                oldMaxLength: 128);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "trn_token",
+                table: "trn_tokens",
+                type: "character(128)",
+                fixedLength: true,
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(128)",
+                oldMaxLength: 128);
+        }
+    }
+}
