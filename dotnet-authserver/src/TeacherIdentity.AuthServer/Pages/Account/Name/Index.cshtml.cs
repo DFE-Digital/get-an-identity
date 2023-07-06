dc5483e567e798a360f3266d13226867e19f5bd1
Andrew Horth <andrew@horth.co.uk>
2023-07-06 12:27:04 +0100

    Amended account DOB and name pages to retain entered data when going back and forth from confirm page (#658)

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/Account/Name/Index.cshtml.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/Account/Name/Index.cshtml.cs
@@ -1,11 +1,11 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TeacherIdentity.AuthServer.Infrastructure.Filters;
 
 namespace TeacherIdentity.AuthServer.Pages.Account.Name;
 
-[BindProperties]
+[VerifyQueryParameterSignature]
 public class Name : PageModel
 {
     private readonly IdentityLinkGenerator _linkGenerator;
@@ -15,18 +15,20 @@ public class Name : PageModel
         _linkGenerator = linkGenerator;
     }
 
-    [BindNever]
     public ClientRedirectInfo? ClientRedirectInfo => HttpContext.GetClientRedirectInfo();
 
+    [BindProperty(SupportsGet = true)]
     [Display(Name = "First name", Description = "Or given names")]
     [Required(ErrorMessage = "Enter your first name")]
     [StringLength(200, ErrorMessage = "First name must be 200 characters or less")]
     public string? FirstName { get; set; }
 
+    [BindProperty(SupportsGet = true)]
     [Display(Name = "Middle name")]
     [StringLength(200, ErrorMessage = "Middle name must be 200 characters or less")]
     public string? MiddleName { get; set; }
 
+    [BindProperty(SupportsGet = true)]
     [Display(Name = "Last name", Description = "Or family names")]
     [Required(ErrorMessage = "Enter your last name")]
     [StringLength(200, ErrorMessage = "Last name must be 200 characters or less")]
@@ -34,9 +36,14 @@ public class Name : PageModel
 
     public void OnGet()
     {
-        FirstName = User.GetFirstName();
-        MiddleName = User.GetMiddleName();
-        LastName = User.GetLastName();
+        if (FirstName is null && LastName is null)
+        {
+            FirstName = User.GetFirstName();
+            MiddleName = User.GetMiddleName();
+            LastName = User.GetLastName();
+        }
+
+        ModelState.Clear();
     }
 
     public IActionResult OnPost()
