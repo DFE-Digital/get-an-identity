6fa0be24f25a1d9af4c72521fc6c11a103e6777c
James Gunn <james@gunn.io>
2023-07-28 16:15:21 +0100

    Fix Create Account after new email entered in TRN token journey (#687)

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/SignIn/TrnToken/Landing.cshtml.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/SignIn/TrnToken/Landing.cshtml.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using TeacherIdentity.AuthServer.Journeys;
+using TeacherIdentity.AuthServer.Models;
 using TeacherIdentity.AuthServer.Oidc;
 using TeacherIdentity.AuthServer.Services.UserSearch;
 
@@ -13,15 +15,18 @@ public class Landing : PageModel
     private const string CurrentStep = TrnTokenSignInJourney.Steps.Landing;
 
     private readonly SignInJourney _journey;
+    private readonly TeacherIdentityServerDbContext _dbContext;
     private readonly TeacherIdentityApplicationManager _applicationManager;
     private readonly IUserSearchService _userSearchService;
 
     public Landing(
         SignInJourney journey,
+        TeacherIdentityServerDbContext dbContext,
         TeacherIdentityApplicationManager applicationManager,
         IUserSearchService userSearchService)
     {
         _journey = journey;
+        _dbContext = dbContext;
         _applicationManager = applicationManager;
         _userSearchService = userSearchService;
     }
@@ -30,7 +35,16 @@ public class Landing : PageModel
 
     public async Task<IActionResult> OnPost()
     {
-        var authenticationState = HttpContext.GetAuthenticationState();
+        var authenticationState = _journey.AuthenticationState;
+
+        // Ensure we've got a verified email populated;
+        // it may have been unset if the user clicked Sign In, entered an email, then came back here without verifying
+        if (!authenticationState.EmailAddressVerified)
+        {
+            var trnToken = await _dbContext.TrnTokens.SingleAsync(t => t.TrnToken == authenticationState.TrnToken);
+            authenticationState.OnEmailSet(trnToken.Email);
+            authenticationState.OnEmailVerified(user: null);
+        }
 
         var users = await _userSearchService.FindUsers(
             authenticationState.FirstName!,
