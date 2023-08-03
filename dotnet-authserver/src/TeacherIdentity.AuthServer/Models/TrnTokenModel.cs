5fc53f548f595377c81c502717fabde906daf5e5
James Gunn <james@gunn.io>
2023-08-03 12:36:47 +0100

    Ask for DOB in TRN token journey if DQT record is missing it

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Models/TrnTokenModel.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Models/TrnTokenModel.cs
@@ -18,5 +18,5 @@ public class EnhancedTrnToken : TrnTokenModel
     public required string FirstName { get; set; }
     public required string? MiddleName { get; set; }
     public required string LastName { get; set; }
-    public required DateOnly DateOfBirth { get; set; }
+    public required DateOnly? DateOfBirth { get; set; }
 }
