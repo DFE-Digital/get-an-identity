d74a4829ccf4310d17755292985fa6d1c0c7c6ec
Andrew Horth <andrew@horth.co.uk>
2023-08-03 16:42:49 +0100

    Small tweak to throw exception if trying to use legacy journey

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Journeys/SignInJourneyProvider.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Journeys/SignInJourneyProvider.cs
@@ -1,3 +1,5 @@
+using TeacherIdentity.AuthServer.Models;
+
 namespace TeacherIdentity.AuthServer.Journeys;
 
 public class SignInJourneyProvider
@@ -6,6 +8,13 @@ public class SignInJourneyProvider
     {
         if (authenticationState.TryGetOAuthState(out var oAuthState) && authenticationState.UserRequirements.RequiresTrnLookup())
         {
+#pragma warning disable CS0612 // Type or member is obsolete
+            if (oAuthState.TrnRequirementType == TrnRequirementType.Legacy)
+            {
+                throw new NotSupportedException("The Legacy journey is no longer supported.");
+            }
+#pragma warning restore CS0612 // Type or member is obsolete
+
             return authenticationState.HasTrnToken ?
                 ActivatorUtilities.CreateInstance<TrnTokenSignInJourney>(httpContext.RequestServices, httpContext) :
                 ActivatorUtilities.CreateInstance<CoreSignInJourneyWithTrnLookup>(httpContext.RequestServices, httpContext);
