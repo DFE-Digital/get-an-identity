a08cc6241a5f8291505dfcecda370b382332d266
James Gunn <james@gunn.io>
2023-10-16 09:59:01 +0100

    Add TRN row to Registration journey Check Answers

--- dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/SignIn/Register/CheckAnswers.cshtml.cs
+++ dotnet-authserver/src/TeacherIdentity.AuthServer/Pages/SignIn/Register/CheckAnswers.cshtml.cs
@@ -20,17 +20,19 @@ public class CheckAnswers : PageModel
 
     public string BackLink => _journey.GetPreviousStepUrl(CurrentStep);
 
-    public bool? RequiresTrnLookup => _journey.AuthenticationState.UserRequirements.RequiresTrnLookup();
+    public bool RequiresTrnLookup => _journey.AuthenticationState.UserRequirements.RequiresTrnLookup();
     public string? EmailAddress => _journey.AuthenticationState.EmailAddress;
     public string? MobilePhoneNumber => _journey.AuthenticationState.MobileNumber is not null ? MobileNumber.Parse(_journey.AuthenticationState.MobileNumber).ToDisplayString() : null;
     public string? FullName => _journey.AuthenticationState.GetName();
     public string? PreferredName => _journey.AuthenticationState.PreferredName;
     public DateOnly? DateOfBirth => _journey.AuthenticationState.DateOfBirth;
-    public bool? HasNationalInsuranceNumberSet => _journey.AuthenticationState.HasNationalInsuranceNumberSet;
+    public bool HasNationalInsuranceNumberSet => _journey.AuthenticationState.HasNationalInsuranceNumberSet;
     public string? NationalInsuranceNumber => _journey.AuthenticationState.NationalInsuranceNumber;
-    public bool? AwardedQtsSet => _journey.AuthenticationState.AwardedQtsSet;
+    public bool AwardedQtsSet => _journey.AuthenticationState.AwardedQtsSet;
     public bool? AwardedQts => _journey.AuthenticationState.AwardedQts;
     public string? IttProviderName => _journey.AuthenticationState.IttProviderName;
+    public bool HasStatedTrnSet => _journey.AuthenticationState.HasTrnSet;
+    public string? StatedTrn => _journey.AuthenticationState.StatedTrn;
 
     public void OnGet()
     {
