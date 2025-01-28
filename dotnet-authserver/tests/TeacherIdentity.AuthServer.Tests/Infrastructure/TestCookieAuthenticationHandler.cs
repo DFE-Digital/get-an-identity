8d68d87c12990d3a735c748d55cc4186500c8bd4
James Gunn <james@gunn.io>
2025-01-28 11:44:17 +0000

    Revert "Upgrade to .NET 8 (#799)" (#800)
    This reverts commit 41bfcd66d6aae5ac907355f2dab69d95da1893ca.

--- dotnet-authserver/tests/TeacherIdentity.AuthServer.Tests/Infrastructure/TestCookieAuthenticationHandler.cs
+++ dotnet-authserver/tests/TeacherIdentity.AuthServer.Tests/Infrastructure/TestCookieAuthenticationHandler.cs
@@ -12,10 +12,11 @@ public class TestCookieAuthenticationHandler : CookieAuthenticationHandler
     private readonly CurrentUserIdContainer _currentUserIdContainer;
 
     public TestCookieAuthenticationHandler(
+        CurrentUserIdContainer currentUserIdContainer,
         IOptionsMonitor<CookieAuthenticationOptions> options,
         ILoggerFactory logger,
         UrlEncoder encoder,
-        CurrentUserIdContainer currentUserIdContainer) : base(options, logger, encoder)
+        ISystemClock clock) : base(options, logger, encoder, clock)
     {
         _currentUserIdContainer = currentUserIdContainer;
     }
